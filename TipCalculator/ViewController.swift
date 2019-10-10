176a7ab30572c1d43ff3d28490d8e3f9d25348c9
Sawsan Attyani <sattyani@lcs.on.ca>
2019-10-10 11:18:19 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Attyani, Sawsan on 2019-10-10.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
